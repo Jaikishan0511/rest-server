a27dfc0d0c922c1310dd91f795ed4b77fece126e
joker0511 <jaikishan.khandelwal@codeastu.com>
2019-08-01 20:45:03 +0530

    added post

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-const postdata = [{ id: 0, title: "post1", description: "hello there" }];
+let postdata = [
+  { id: 0, title: "post0", description: "description0" },
+  { id: 1, title: "post0", description: "description0" }
+];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get("/post", (req, res) => {
@@ -14,5 +17,22 @@ app.get("/post/:id", function(req, res) {
   console.log(req.params);
   res.send("");
 });
-//app.post('/notes', (req, res) => {
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post("/post", (req, res) => {
+  if (req.body.title && req.body.description) {
+    const newElement = {
+      id: postdata.length,
+      title: req.body.title,
+      description: req.body.description
+    };
+    postdata.push(newElement);
+    return res.json({
+      data: postdata
+    });
+  }
+  res.status(500).send({
+    message: "invalid data"
+  });
+});
+app.listen(port, () =>
+  console.log(`Example app listening oappn port ${port}!`)
+);
