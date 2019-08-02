9a8d77999113f1bdd6384f1ff82319b2eed4b28f
joker0511 <jaikishan.khandelwal@codeastu.com>
2019-08-02 18:47:44 +0530

    added put and delete

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-let postdata = [
+var postdata = [
   { id: 0, title: "post0", description: "description0" },
   { id: 1, title: "post0", description: "description0" }
 ];
@@ -27,6 +27,31 @@ app.post("/post", (req, res) => {
     postdata.push(newElement);
     return res.json({
       data: postdata
+    });
+  }
+  res.status(500).send({
+    message: "invalid data"
+  });
+});
+app.put("/post", (req, res) => {
+  index = postdata.findIndex(x => x.id === 2);
+  postdata.splice(index, 1, {
+    id: 2,
+    title: "new title",
+    description: "new desc"
+  });
+  return res.json({
+    data: postdata
+  });
+});
+app.delete("/post/:id", (req, res) => {
+  req.params.id = Number(req.params.id);
+  console.log(typeof req.params.id);
+  index = postdata.findIndex(x => x.id === req.params.id);
+  if (index > -1) {
+    postdata.splice(index, 1);
+    return res.json({
+      success: "true"
     });
   }
   res.status(500).send({
